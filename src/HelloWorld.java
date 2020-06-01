405cf7d2dc09de0568cbf069a0e22ce56d8b78f5
Leroy Grant <lgrant146@gmail.com>
2020-06-01 16:29:50 -0500

    java I exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,42 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 1134;
+        float myNumber = 2.356f;
+
+        System.out.println(myFavoriteNumber);
+        System.out.println("Hello, World!");
+        String myString = "Hello";
+        myString = "3.14159";
+        System.out.println(myString);
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= y;
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        x /= y;
+        y = y - x;
+        y -= x;
+
+
+    }
+}
+
