296b8fa42dbd5918589b573629123a7ad5d4bae3
Leroy Grant <lgrant146@gmail.com>
2020-06-04 10:25:20 -0500

    fizzbuzz done

--- src/ControlflowExercises.java
+++ src/ControlflowExercises.java
@@ -1,15 +1,26 @@
 public class ControlflowExercises {
     public static void main(String[] args) {
-        for (int i = 5; i <= 15; i += 1) {
-            System.out.println(i);
-        }
+//        for (int i = 5; i <= 15; i += 1) {
+//            System.out.println(i);
+//        }
+//
+//        for (int k = 0; k <= 100; k += 2) {
+//            System.out.println(k);
+//        }
+//
+//        for (long j = 2; j < 1000000; j *= j) {
+//            System.out.println(j);
+//        }
+        for(int l = 1; l <= 100; l += 1){
+            if (l % 15 == 0)
+                System.out.print("FizzBuzz" + " ");
+            else if (l % 5 == 0)
+                System.out.println("Buzz"+" ");
+            else
+                System.out.print(l +" ");
 
-        for (int k = 0; k <= 100; k += 2) {
-            System.out.println(k);
-        }
 
-        for (long j = 2; j < 1000000; j *= j) {
-            System.out.println(j);
+
         }
     }
 }
