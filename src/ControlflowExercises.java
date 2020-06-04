c70c402b9c9b6795f0d46213fd7f88f40f8c714b
Leroy Grant <lgrant146@gmail.com>
2020-06-04 11:38:12 -0500

    table done

--- src/ControlflowExercises.java
+++ src/ControlflowExercises.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ControlflowExercises {
     public static void main(String[] args) {
 //        for (int i = 5; i <= 15; i += 1) {
@@ -11,16 +12,25 @@ public class ControlflowExercises {
 //        for (long j = 2; j < 1000000; j *= j) {
 //            System.out.println(j);
 //        }
-        for(int l = 1; l <= 100; l += 1){
-            if (l % 15 == 0)
-                System.out.print("FizzBuzz" + " ");
-            else if (l % 5 == 0)
-                System.out.println("Buzz"+" ");
-            else
-                System.out.print(l +" ");
+//        for(int l = 1; l <= 100; l += 1){
+//            if (l % 15 == 0)
+//                System.out.print("FizzBuzz" + " ");
+//            else if (l % 5 == 0)
+//                System.out.println("Buzz"+" ");
+//            else
+//                System.out.print(l +" ");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter number");
+        int num = userInput.nextInt();
 
-
-
+        System.out.println("Here is your table");
+        System.out.println("number     | squared      | cubed");
+        System.out.println("------     | -------      | -----");
+        for(int y = 1; y <= num; y += 1){
+            System.out.printf("%d          | %d           | %d%n", y, y*y, y*y*y);
         }
+
+
+
     }
 }
