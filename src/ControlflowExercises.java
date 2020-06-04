597792fd75c6672598aaea31ae71617a8fed1a20
Leroy Grant <lgrant146@gmail.com>
2020-06-04 10:11:34 -0500

    refactored do while loops to for loops

--- src/ControlflowExercises.java
+++ src/ControlflowExercises.java
@@ -1,14 +1,15 @@
 public class ControlflowExercises {
-    public static void main(String[] args){
-        for(int i = 5; i <= 15; i += 1){
-        System.out.println(i);
+    public static void main(String[] args) {
+        for (int i = 5; i <= 15; i += 1) {
+            System.out.println(i);
         }
-       long j = 2;
 
-do {
-       System.out.println(j);
-     j *= j;
+        for (int k = 0; k <= 100; k += 2) {
+            System.out.println(k);
+        }
 
-    } while (j < 1000000);
+        for (long j = 2; j < 1000000; j *= j) {
+            System.out.println(j);
+        }
     }
 }
