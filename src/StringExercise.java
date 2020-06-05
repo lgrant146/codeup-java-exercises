eb4045df156e3686f2ab4438ac94ded1178b5ab0
Leroy Grant <lgrant146@gmail.com>
2020-06-05 10:21:26 -0500

    first part of string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String  message;
+        message = "We don't need no education \nWe don't need no thought control";
+        System.out.println(message);
+    }
+}
